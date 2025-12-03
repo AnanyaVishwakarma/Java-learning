f56d5a14d12ff8ebcbdfde73abce4afb4ebb7d86
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-12-03 22:56:39 +0530

    Update AnnotationsInJava.java

--- AnnotationsInJava.java
+++ AnnotationsInJava.java
@@ -1,3 +1,5 @@
+/*Annotations are special notes you add to your Java code. They start with the @ symbol.
+They don't change how your program runs, but they give extra information to the compiler or tools. */
 class A{
 public void showB(){
     System.out.println("In A");
@@ -17,4 +19,5 @@ public static void main(String[] args) {
 }
 
 }
+
 
