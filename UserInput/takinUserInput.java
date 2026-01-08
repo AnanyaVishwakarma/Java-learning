f4bef6ef54e326985454889805e1665f7b5ce748
AnanyaVishwakarma <vananya557@gmail.com>
2026-01-08 23:06:02 +0530

    Commented on lines

--- UserInput/takinUserInput.java
+++ UserInput/takinUserInput.java
@@ -2,9 +2,13 @@ import java.io.*;
 class takinUserInput {
 public static void main(String[] args) throws IOException {
 
-    
+    //translating raw keyboard bytes into more readable characters
+    InputStreamReader in = new InputStreamReader(System.in);//System.in reads the input
+    //reads input efficiently using buffer; Reads the entire line at once using readLine()
+    BufferedReader bf =  new BufferedReader(in);
 
-    int num = System.in.read();
+
+    int num = Integer.parseInt(bf.readLine());
     System.out.println(num);   
 
 }    
