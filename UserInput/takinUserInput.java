a8a07d98c456321885e80d83582ce994ae52f44a
AnanyaVishwakarma <vananya557@gmail.com>
2026-01-09 23:22:43 +0530

    Scanner for scnaning user input

--- UserInput/takinUserInput.java
+++ UserInput/takinUserInput.java
@@ -1,14 +1,19 @@
 import java.io.*;
+import java.util.Scanner;
 class takinUserInput {
 public static void main(String[] args) throws IOException {
 
-    //translating raw keyboard bytes into more readable characters
-    InputStreamReader in = new InputStreamReader(System.in);//System.in reads the input
-    //reads input efficiently using buffer; Reads the entire line at once using readLine()
-    BufferedReader bf =  new BufferedReader(in);
+    // //translating raw keyboard bytes into more readable characters
+    // InputStreamReader in = new InputStreamReader(System.in);//System.in reads the input
+    // //reads input efficiently using buffer; Reads the entire line at once using readLine()
+    // BufferedReader bf =  new BufferedReader(in);
+
+    Scanner sc = new Scanner(System.in);
 
 
-    int num = Integer.parseInt(bf.readLine());
+
+    // int num = Integer.parseInt(bf.readLine());
+    int num = sc.nextInt();
     System.out.println(num);   
 
 }    
